a3cae99a6ed547943f3596aa92664477b393bfb7
Neptune <eugeneogongo@live.com>
2019-05-13 12:11:28 +0300

    Register page added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fix Nairobi</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{\Illuminate\Support\Facades\URL::asset("css/app.css")}}" rel="stylesheet"/>
+
+
+</head>
+
+<body>
+@include('layout.header')
+<div id="report" class="container">
+    <h1>Report, view, or discuss local problems</h1>
+    <h2>(like graffiti, fly tipping, broken paving slabs, or street lighting)</h2>
+    <form>
+
+    </form>
+</div>
+
+</body>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,14 @@
+<!-- Navigation -->
+<nav class="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="#">Start Bootstrap</a>
+
+        <a href="#">Report Problem</a>
+        <a href="">Sign in</a>
+        <a href="#">Help</a>
+
+
+        <a class="btn btn-primary" href="#">Sign In</a>
+    </div>
+</nav>
+<!-- Navigation -->
