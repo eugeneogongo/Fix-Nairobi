7baa129944ecd5df9b60233e85c795279e63e0cc
Neptune <eugeneogongo@live.com>
2019-05-23 17:12:54 +0300

    Description of type of Issue is  now unique

---
+++ database/migrations/2019_05_23_115739_add_unique_to_issue_type.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUniqueToIssueType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('type_issues',function (Blueprint $table){
+        $table->string('desc')->unique()->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
