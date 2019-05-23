bd06378b21f8808d54f2c41fc901fb2b5be44ce9
Neptune <eugeneogongo@live.com>
2019-05-23 12:57:58 +0300

    Migration for Tracking issues

---
+++ database/migrations/2019_05_23_070636_issue_status.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IssueStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::Create('IssueStatus',function (Blueprint $table){
+           $table->integer('issueid')->unsigned();
+           $table->string('status')->default('Not Fixed');
+           $table->foreign('issueid')->references('id')->on('problems');
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('issuestatus');
+    }
+}
