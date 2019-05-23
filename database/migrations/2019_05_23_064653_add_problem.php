3a00885aed2383c8e63cbafd4ac1e9cc29545656
Neptune <eugeneogongo@live.com>
2019-05-23 12:57:17 +0300

    Migration for problems

---
+++ database/migrations/2019_05_23_064653_add_problem.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProblem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('problems',function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('userid')->unsigned();
+
+            $table->string('location')->nullable(false);
+            $table->string('Title')->nullable(false);
+            $table->integer('issueid')->unsigned();
+
+            $table->string('landmark');
+            $table->string('moredetails');
+            $table->timestamps();
+            $table->foreign('issueid')->references('id')->on('type_issues');
+            $table->foreign('userid')->references('id')->on('users');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('problems');
+    }
+}
