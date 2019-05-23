97bd313d7aa87047c227839d7042e3861de8865e
Neptune <eugeneogongo@live.com>
2019-05-23 17:14:22 +0300

    Admin can now add Type of Issues to be reported

--- resources/views/admin/newissue.blade.php
+++ resources/views/admin/newissue.blade.php
@@ -1,1 +1,26 @@
-<?php
+@extends('layouts.adminmaster')
+
+@section('content')
+    <div class="card shadow mx-5">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Add new Type of Problem</h6>
+        </div>
+        <div class="card-body">
+            <form method="post" action="{{route('createissue')}}">
+                @csrf
+                <div class="form-group">
+
+                    <input name="desc" class="form-control" autofocus required placeholder="Issue Type">
+
+
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">
+                    Create Issue Type
+                </button>
+            </form>
+        </div>
+    </div>
+
+    </div>
+@endsection
+
