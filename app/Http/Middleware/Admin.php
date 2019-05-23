07ccf6a688ed9c629d379439d13ebc19a4a73d77
Neptune <eugeneogongo@live.com>
2019-05-23 13:01:03 +0300

    Checks if a user is admin otherwise throw 404

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->user()->isAdmin == 1) {
+            return $next($request);
+        }
+        abort(404);
+    }
+}
