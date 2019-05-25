0889b9a42336f99c766a902b16d1c2a6b080e0cb
Neptune <eugeneogongo@live.com>
2019-05-25 21:52:39 +0300

    Recent issues done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('css')
     <link href="{{\Illuminate\Support\Facades\URL::asset("css/mystyle.css")}}" rel="stylesheet"/>
+    <link href="{{ asset ('css/dashboard.css')}}" rel="stylesheet" type="text/css">
 @endsection
 @section('content')
 <section id="report" class="text-center">
@@ -16,16 +17,38 @@
             <div class="container">
                 <h1>How to Report an Issue</h1>
                 <ol class="problem-list">
-                    <li>Enter a nearby UK postcode, or street name and area</li>
+                    <li>Click on the Post Problem Button</li>
                     <li>Locate the problem on a map of the area</li>
                     <li>Enter details of the problem</li>
-                    <li>We send it to the council on your behalf</li>
+                    <li>Submit the Problem</li>
                 </ol>
             </div>
             <div>
                 <h1>Recent Issues Reported</h1>
-            </div>
-        </div>
+                <div class="container">
+                    <div class="row">
+                        @php
+                            use Illuminate\Support\Facades\DB;$problems  =DB::table('problems')
+                            ->select('problems.id as id','moredetails as detail','title','Location',"type_issues.desc",'path')
+                            ->join('issuestatus','problems.id','=','issuestatus.issueid')
+                            ->join("type_issues","type_issues.id","=","problems.issueid")
+                            ->join("photos",'problems.id','=','photos.issueid')
+                            ->where('status','=','not fixed')->limit(2)-> get();
+
+                            foreach ($problems as $prob){
+                            echo('<div class="card issuecard">');
+                            echo('
+                            <img class="card-img-top" src='.asset('images/'.$prob->path).' />
+                           <div class="card-body">
+                            <h5 class="card-title">'.$prob->title.'</h5> Was Reported at '.$prob->detail.'<br>
+                            <a href=/viewissue/'.$prob->id.' class="btn btn-primary">View Issue</a>
+                      </div></div>');
+                            }
+                        @endphp
+                    </div>
+                    <div>
+                    </div>
+                </div>
     </section>
 </div>
 
