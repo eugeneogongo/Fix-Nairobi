360481dd902fe1edc81b124376fcee1feba34028
Neptune <eugeneogongo@live.com>
2019-06-14 17:31:50 +0300

    Change model location

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -19,7 +19,7 @@
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Problems Reported</div>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                           {{ _($count = \App\Problem::all()->count()) + 10}}
+                                           {{ _($count = \FixNairobi\Problem::all()->count()) + 10}}
                                         </div>
                                     </div>
                                 </div>
