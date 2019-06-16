ef2e80db8f10e0bc3de54c874ec9ea5fe5740b29
Neptune <eugeneogongo@live.com>
2019-06-16 18:23:28 +0300

    Removed Boilerplaters

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -63,7 +63,7 @@
                     </div>
 
                     <!-- Pending Requests Card Example -->
-                    <div class="col-xl-3 col-md-6 mb-4">
+                    <div>
                         <div class="card border-left-warning shadow h-100 py-2">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
@@ -82,7 +82,7 @@
                 <div class="row">
 
                     <!-- Area Chart -->
-                    <div class="col-xl-8 col-lg-7">
+                    <div>
                         <div class="card shadow mb-4">
                             <!-- Card Header - Dropdown -->
                             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -98,69 +98,30 @@
                                     <div class="container">
 
 
-                                    <div class="row">
+                                        <div class="row justify-content-center">
                                 @php
-                                $problems  =\Illuminate\Support\Facades\DB::table('problems')
-                                ->select('problems.id as id','moredetails as detail','Title','location',"Type_issues.desc",'path')
-                                ->join('IssueStatus','problems.id','=','IssueStatus.issueid')
-                                ->join("Type_issues","Type_issues.id","=","problems.issueid")
-                                ->join("photos",'problems.id','=','photos.issueid')
-                                ->where('status','=','not fixed')->limit(12)-> get();
+                                    use Illuminate\Support\Facades\DB;$problems  =DB::table('problems')
+                                    ->select('problems.id as id','moredetails as detail','Title','location',"Type_issues.desc",'path')
+                                    ->join('IssueStatus','problems.id','=','IssueStatus.issueid')
+                                    ->join("Type_issues","Type_issues.id","=","problems.issueid")
+                                    ->join("photos",'problems.id','=','photos.issueid')
+                                    ->where('status','=','not fixed')->limit(12)-> get();
 
-                                foreach ($problems as $prob){
-                                echo('<div class="card issuecard">');
-                                echo('
-                                <img class="card-img-top" src='.asset('images/'.$prob->path).' />
-                               <div class="card-body">
-                                <h5 class="card-title">'.$prob->Title.'</h5> Was Reported at '.$prob->detail.'<br>
-                                <a href=viewissue/'.$prob->id.' class="btn btn-primary">View Issue</a>
-                          </div></div>');
-                                }
+                                    foreach ($problems as $prob){
+                                    echo('<div class="card issuecard">');
+                                    echo('
+                                    <img class="card-img-top" src='.asset('images/'.$prob->path).' />
+                                   <div class="card-body">
+                                    <h5 class="card-title">'.$prob->Title.'</h5> Was Reported at '.$prob->detail.'<br>
+                                    <a href=viewissue/'.$prob->id.' class="btn btn-primary">View Issue</a>
+                              </div></div>');
+                                    }
                                 @endphp
                         </div>
                     </div>
                         </div>
                     </div>
 
-                    <!-- Pie Chart -->
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="card shadow mb-4">
-                            <!-- Card Header - Dropdown -->
-                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                                <div class="dropdown no-arrow">
-                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                                        <div class="dropdown-header">Dropdown Header:</div>
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Card Body -->
-                            <div class="card-body">
-                                <div class="chart-pie pt-4 pb-2">
-                                    <canvas id="myPieChart"></canvas>
-                                </div>
-                                <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-primary"></i> Direct
-                    </span>
-                                    <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Social
-                    </span>
-                                    <span class="mr-2">
-                      <i class="fas fa-circle text-info"></i> Referral
-                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <!-- Content Row -->
                 <div class="row">
