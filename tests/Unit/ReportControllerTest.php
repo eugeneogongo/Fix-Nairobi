ccdd4e356457eb51f544cc6865f91d7aeaf77fc6
Neptune <eugeneogongo@live.com>
2019-06-29 21:43:24 +0300

    unit tests

--- tests/Unit/ReportControllerTest.php
+++ tests/Unit/ReportControllerTest.php
@@ -29,6 +29,46 @@ class ReportControllerTest extends TestCase
         $response = $this->actingAs($user)->get('/reportproblem');
         $response->assertStatus(200);
     }
-/*
-    public function testReportIssue()*/
+
+    public function testReportIssue()
+    {
+        $this->withoutMiddleware();
+        $user = factory(User::class)->create([
+            'isAdmin' => '1'
+        ]);
+        $stub = __DIR__ . '/test.jpg';
+        $name = str_random(8) . '.jpg';
+        $path = sys_get_temp_dir() . '/' . $name;
+
+        copy($stub, $path);
+
+        $file = new UploadedFile($path, $name, filesize($path), null, true);
+
+        $issue = new TypeIssues();
+        $issue->desc = "test";
+        $id = $issue->save();
+
+        $this->assertDatabaseHas('Type_issues', [
+            'desc' => 'test'
+        ]);
+
+        $response = $this->actingAs($user)->post('/reportproblem', [
+            'location' => '(123,124)',
+            'issueid' => $id,
+            'landmark' => 'gachoro',
+            'moredetails' => 'moredetails',
+            'desc' => 'Hello world'
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('problems', [
+             'issueid' => '1',
+         ]);
+        $this->assertDatabaseHas('IssueStatus', [
+            'status' => 'Not Fixed',
+            'issueid' => '1'
+        ]);
+        $content = json_decode($response->getContent());
+        $this->assertObjectHasAttribute('status', $content);
+    }
 }
