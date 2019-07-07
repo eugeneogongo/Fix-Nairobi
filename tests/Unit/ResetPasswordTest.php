b972e06ee28f0a0f4421e2ce3a63ba6a1dadd2e9
Neptune <eugeneogongo@live.com>
2019-07-07 16:13:54 +0300

    Tests for ResetPassword flow

---
+++ tests/Unit/ResetPasswordTest.php
@@ -0,0 +1,140 @@
+<?php
+
+use FixNairobi\Http\Middleware\VerifyCsrfToken;
+use FixNairobi\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Tests\TestCase;
+
+class ResetPasswordTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserCanViewAPasswordResetForm()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->get($this->passwordResetGetRoute($token = $this->getValidToken($user)));
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.passwords.reset');
+        $response->assertViewHas('token', $token);
+    }
+
+    protected function passwordResetGetRoute($token)
+    {
+        return route('password.reset', $token);
+    }
+
+    protected function getValidToken($user)
+    {
+        return Password::broker()->createToken($user);
+    }
+
+    public function testUserCannotViewAPasswordResetFormWhenAuthenticated()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get($this->passwordResetGetRoute($this->getValidToken($user)));
+        $response->assertRedirect($this->guestMiddlewareRoute());
+    }
+
+    protected function guestMiddlewareRoute()
+    {
+        return route('home');
+    }
+
+    public function testUserCanResetPasswordWithValidToken()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        Event::fake();
+        $user = factory(User::class)->create();
+        $response = $this->post($this->passwordResetPostRoute(), [
+            'token' => $this->getValidToken($user),
+            'email' => $user->email,
+            'password' => 'new-awesome-password',
+            'password_confirmation' => 'new-awesome-password',
+        ]);
+        $response->assertRedirect($this->successfulPasswordResetRoute());
+        $this->assertEquals($user->email, $user->fresh()->email);
+        $this->assertTrue(Hash::check('new-awesome-password', $user->fresh()->password));
+        $this->assertAuthenticatedAs($user);
+        Event::assertDispatched(PasswordReset::class, function ($e) use ($user) {
+            return $e->user->id === $user->id;
+        });
+    }
+
+    protected function passwordResetPostRoute()
+    {
+        return '/password/reset';
+    }
+
+    protected function successfulPasswordResetRoute()
+    {
+        return route('home');
+    }
+
+    public function testUserCannotResetPasswordWithInvalidToken()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $user = factory(User::class)->create([
+            'password' => Hash::make('old-password'),
+        ]);
+        $response = $this->from($this->passwordResetGetRoute($this->getInvalidToken()))->post($this->passwordResetPostRoute(), [
+            'token' => $this->getInvalidToken(),
+            'email' => $user->email,
+            'password' => 'new-awesome-password',
+            'password_confirmation' => 'new-awesome-password',
+        ]);
+        $response->assertRedirect($this->passwordResetGetRoute($this->getInvalidToken()));
+        $this->assertEquals($user->email, $user->fresh()->email);
+        $this->assertTrue(Hash::check('old-password', $user->fresh()->password));
+        $this->assertGuest();
+    }
+
+    protected function getInvalidToken()
+    {
+        return 'invalid-token';
+    }
+
+    public function testUserCannotResetPasswordWithoutProvidingANewPassword()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $user = factory(User::class)->create([
+            'password' => Hash::make('old-password'),
+        ]);
+        $response = $this->from($this->passwordResetGetRoute($token = $this->getValidToken($user)))->post($this->passwordResetPostRoute(), [
+            'token' => $token,
+            'email' => $user->email,
+            'password' => '',
+            'password_confirmation' => '',
+        ]);
+        $response->assertRedirect($this->passwordResetGetRoute($token));
+        $response->assertSessionHasErrors('password');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertEquals($user->email, $user->fresh()->email);
+        $this->assertTrue(Hash::check('old-password', $user->fresh()->password));
+        $this->assertGuest();
+    }
+
+    public function testUserCannotResetPasswordWithoutProvidingAnEmail()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $user = factory(User::class)->create([
+            'password' => Hash::make('old-password'),
+        ]);
+        $response = $this->from($this->passwordResetGetRoute($token = $this->getValidToken($user)))->post($this->passwordResetPostRoute(), [
+            'token' => $token,
+            'email' => '',
+            'password' => 'new-awesome-password',
+            'password_confirmation' => 'new-awesome-password',
+        ]);
+        $response->assertRedirect($this->passwordResetGetRoute($token));
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertEquals($user->email, $user->fresh()->email);
+        $this->assertTrue(Hash::check('old-password', $user->fresh()->password));
+        $this->assertGuest();
+    }
+}
