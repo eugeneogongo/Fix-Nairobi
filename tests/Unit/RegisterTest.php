a206a4c641d4ca4a912eccac8053cdd5bc22bf3e
Neptune <eugeneogongo@live.com>
2019-07-07 15:47:18 +0300

    Tests for Register flow

---
+++ tests/Unit/RegisterTest.php
@@ -0,0 +1,180 @@
+<?php
+
+use FixNairobi\Http\Middleware\VerifyCsrfToken;
+use FixNairobi\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserCanViewARegistrationForm()
+    {
+        $response = $this->get($this->registerGetRoute());
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.register');
+    }
+
+    protected function registerGetRoute()
+    {
+        return route('register');
+    }
+
+    public function testUserCannotViewARegistrationFormWhenAuthenticated()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->actingAs($user)->get($this->registerGetRoute());
+        $response->assertRedirect($this->guestMiddlewareRoute());
+    }
+
+    protected function guestMiddlewareRoute()
+    {
+        return route('home');
+    }
+
+    public function testUserCanRegister()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        Event::fake();
+        $response = $this->post($this->registerPostRoute(), [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'i-love-laravel',
+            'password_confirmation' => 'i-love-laravel',
+        ]);
+        $response->assertRedirect('/');
+        $this->assertCount(1, $users = User::all());
+        $this->assertAuthenticatedAs($user = $users->first());
+        $this->assertEquals('John Doe', $user->name);
+        $this->assertEquals('john@example.com', $user->email);
+        $this->assertTrue(Hash::check('i-love-laravel', $user->password));
+        Event::assertDispatched(Registered::class, function ($e) use ($user) {
+            return $e->user->id === $user->id;
+        });
+    }
+
+    protected function registerPostRoute()
+    {
+        return route('register');
+    }
+
+    public function testUserCannotRegisterWithoutName()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $response = $this->from($this->registerGetRoute())->post($this->registerPostRoute(), [
+            'name' => '',
+            'email' => 'john@example.com',
+            'password' => 'i-love-laravel',
+            'password_confirmation' => 'i-love-laravel',
+        ]);
+        $users = User::all();
+        $this->assertCount(0, $users);
+        $response->assertRedirect($this->registerGetRoute());
+        $response->assertSessionHasErrors('name');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function testUserCannotRegisterWithoutEmail()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $response = $this->from($this->registerGetRoute())->post($this->registerPostRoute(), [
+            'name' => 'John Doe',
+            'email' => '',
+            'password' => 'i-love-laravel',
+            'password_confirmation' => 'i-love-laravel',
+        ]);
+        $users = User::all();
+        $this->assertCount(0, $users);
+        $response->assertRedirect($this->registerGetRoute());
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function testUserCannotRegisterWithInvalidEmail()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $response = $this->from($this->registerGetRoute())->post($this->registerPostRoute(), [
+            'name' => 'John Doe',
+            'email' => 'invalid-email',
+            'password' => 'i-love-laravel',
+            'password_confirmation' => 'i-love-laravel',
+        ]);
+        $users = User::all();
+        $this->assertCount(0, $users);
+        $response->assertRedirect($this->registerGetRoute());
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function testUserCannotRegisterWithoutPassword()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $response = $this->from($this->registerGetRoute())->post($this->registerPostRoute(), [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => '',
+            'password_confirmation' => '',
+        ]);
+        $users = User::all();
+        $this->assertCount(0, $users);
+        $response->assertRedirect($this->registerGetRoute());
+        $response->assertSessionHasErrors('password');
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function testUserCannotRegisterWithoutPasswordConfirmation()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $response = $this->from($this->registerGetRoute())->post($this->registerPostRoute(), [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'i-love-laravel',
+            'password_confirmation' => '',
+        ]);
+        $users = User::all();
+        $this->assertCount(0, $users);
+        $response->assertRedirect($this->registerGetRoute());
+        $response->assertSessionHasErrors('password');
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function testUserCannotRegisterWithPasswordsNotMatching()
+    {
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+        $response = $this->from($this->registerGetRoute())->post($this->registerPostRoute(), [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'i-love-laravel',
+            'password_confirmation' => 'i-love-symfony',
+        ]);
+        $users = User::all();
+        $this->assertCount(0, $users);
+        $response->assertRedirect($this->registerGetRoute());
+        $response->assertSessionHasErrors('password');
+        $this->assertTrue(session()->hasOldInput('name'));
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    protected function successfulRegistrationRoute()
+    {
+        return route('home');
+    }
+}
