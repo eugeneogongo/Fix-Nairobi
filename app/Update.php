6be8854cfff4d18b5e569cafc07a45fe9cdedfe2
Neptune <eugeneogongo@live.com>
2019-08-01 14:06:25 +0300

    Update Model

---
+++ app/Update.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Developed by Eugene Ogongo on 8/1/19 2:06 PM
+ * Author Email: eugeneogongo@live.com
+ * Last Modified 7/24/19 7:26 PM
+ * Copyright (c) 2019 . All rights reserved
+ */
+
+namespace FixNairobi;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Update extends Model
+{
+    //
+    protected $table = "updates";
+}
