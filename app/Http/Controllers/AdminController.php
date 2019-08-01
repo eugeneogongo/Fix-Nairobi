ce283f45a5e64c8c1eec5fdcfef9f14b10963f96
Neptune <eugeneogongo@live.com>
2019-08-01 14:04:34 +0300

    Dashboard cleaning

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,14 +1,17 @@
 <?php
 /**
- * Developed by Eugene Ogongo on 7/20/19 10:44 AM
+ * Developed by Eugene Ogongo on 8/1/19 2:04 PM
  * Author Email: eugeneogongo@live.com
- * Last Modified 7/20/19 10:41 AM
+ * Last Modified 8/1/19 2:04 PM
  * Copyright (c) 2019 . All rights reserved
  */
 
 namespace FixNairobi\Http\Controllers;
 
 use FixNairobi\Feedback;
+use FixNairobi\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -25,14 +28,36 @@ class AdminController extends Controller
 
     public function admin()
     {
-        return view('admin.dashboard');
+        $feedback = Feedback::all()->last();
+        $stats = DB::select(DB::raw("SELECT type_issues.desc,count(*) as \"count\" from type_issues inner join problems on problems.issueid = type_issues.id GROUP by type_issues.desc"));
+        return view('admin.dashboard')->with(['feedback' => $feedback, "stats" => $stats]);
     }
 
     public  function  feedbacks(){
         $feedbacks = Feedback::all();
 
+
         return view('admin.allfeedbacks')->with([
             'feedbacks'=>$feedbacks]);
+    }
+
+    public function adminpage()
+    {
+        return view('admin.newadmin');
+    }
+
+    public function createadmin(Request $request)
+    {
+
+        $user = User::all()->where("email", "=", $request->email)->first();
+        if ($user != null) {
+            $user->isAdmin = true;
+            if ($user->save()) {
+                return view("admin.newadmin")->with(["success" => true]);
+            }
+        } else {
+            return view("admin.newadmin")->with(["error" => true]);
+        }
     }
 
 }
